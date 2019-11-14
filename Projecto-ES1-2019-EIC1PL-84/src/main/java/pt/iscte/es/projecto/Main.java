1cbedc4dba85292035709616e3d8c8de9322d1c5
eduardo <emnvl@iscte-iul.pt>
2019-11-14 18:29:42 +0000

    Initial commit

---
+++ Projecto-ES1-2019-EIC1PL-84/src/main/java/pt/iscte/es/projecto/Main.java
@@ -0,0 +1,27 @@
+package pt.iscte.es.projecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ){
+        System.out.println( "=====================================================\n"
+        				  + "================== COMPARATOR TOOL ==================\n"
+        				  + "==       ISCTE-IUL -- Engenharia de Software       ==\n"
+        				  + "==                 Projecto final                  ==\n"
+        				  + "==                                                 ==\n"
+        				  + "==    Autoria dos alunos:                          ==\n"
+        				  + "==      • Eduardo Vidal    | 70259                 ==\n"
+        				  + "==      • Guilherme Simões | 78149                 ==\n"
+        				  + "==      • Mário Botelho    | 69464                 ==\n"
+        				  + "==      • Pedro Bernardo   | 73158                 ==\n"
+        				  + "==      • Pedro Martins    | 78117                 ==\n"
+        				  + "==                                                 ==\n"
+        				  + "=====================================================\n"
+        				  + "=====================================================\n\n" );
+        
+        System.out.println("Running from: " + System.getProperty("user.dir"));
+    }
+}
